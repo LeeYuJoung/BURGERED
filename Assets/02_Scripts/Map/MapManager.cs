f82d2b1c18299162cf922173eae2ce71408b7406
LEYuJeong <nania000822@naver.com>
2024-03-31 23:01:04 +0900

    Add Assets

--- Assets/02_Scripts/Map/MapManager.cs
+++ Assets/02_Scripts/Map/MapManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] gameObjectsPrefabs;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
