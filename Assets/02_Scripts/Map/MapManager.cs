0ddc719c0878bf3088f30e522868a439ad041038
LEYuJeong <nania000822@naver.com>
2024-04-04 22:34:33 +0900

    MapGenerator

--- Assets/02_Scripts/Map/MapManager.cs
+++ Assets/02_Scripts/Map/MapManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
+    private string dataPath;
+    private List<Dictionary<string, object>> data;
+
     public GameObject[] gameObjectsPrefabs;
 
     void Start()
     {
-        
+        LoadMapData(Random.Range(1, 10));
+
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 15; j++)
+            {
+                GameObject floor = Instantiate(gameObjectsPrefabs[0]);
+                floor.transform.parent = GameObject.Find("Maps").transform;
+                floor.gameObject.name = floor.tag + $"( {j}, {i} )";
+                floor.transform.localPosition = new Vector3(j, 0, -i);
+            }
+        }
+
+        MakeMap();
     }
 
-    void Update()
+    public void LoadMapData(int stringNum)
     {
-        
+        dataPath = "Map/Map_" + stringNum;
+        data = CSVReader.Read(dataPath);
+    }
+
+    public void MakeMap()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 15; j++)
+            {
+                int dataSet = (int)data[i][j.ToString()];
+
+                if(dataSet != 0)
+                {
+                    GameObject mapObject = Instantiate(gameObjectsPrefabs[dataSet]);
+                    mapObject.transform.parent = GameObject.Find("Maps").transform;
+                    mapObject.gameObject.name = mapObject.tag + $"( {j}, {i} )";
+                    mapObject.transform.localPosition = new Vector3(j, 0, -i);
+                }
+            }
+        }
     }
 }
